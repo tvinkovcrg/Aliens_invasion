54250df44ce4e7d101d2ccc7b3fff9734ceda8da
Roman Babenko <tvinkovcrg@gmail.com>
2024-06-27 20:50:55 +0300

    add player_ship.bmp and settings.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Загальний клас, що керує ресурсам та поведінкою гри"""
 
@@ -9,19 +11,23 @@ class AlienInvasion:
         """Ініціалізувати гру, створити ресурси гри"""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width , self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
 
     def run_game(self):
         """Розпочати головний цикл гри"""
         while True:
             # Слідкувати за подіями миші та клавіатури
-            for event in pygame.event.get()
+            for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            #Показувати останній намальований екран
-            pygame.display.flip()
+                self.screen.fill(self.settings.bg_color)
+
+                #Показувати останній намальований екран
+                pygame.display.flip()
 
 if __name__ == '__main__':
     #Створити екземпляр гри та запустити гру
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """Клас для збереження всіх налаштувань гри"""
+
+    def __init__(self):
+        """Ініціалізувати налаштування гри"""
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (120, 120, 120)
